720e1bc394ca600e9f9e3f5944e87371d67f6cfa
trhowell <trhowell@ucdavis.edu>
2014-04-21 19:22:59 -0700

    Added some more graphs to look into different training runs, cleaned some things up

--- Moving_and_Elapsed_time.R
+++ Moving_and_Elapsed_time.R
@@ -3,6 +3,7 @@
 #input is all_data.csv output of create_data_mod.R
 
 require(ggplot2)
+require(gridExtra)
 setwd("location/of/csvfile")
 
 #Read in all Strava data
@@ -12,10 +13,10 @@ alldata$workout_type <- as.factor(alldata$workout_type)
 rundata <- alldata[which(alldata$type=="Run" & alldata$elapsed_time < 90000 & alldata$moving_time<=alldata$elapsed_time),]
 
 #subset races for run data
-runrace <- rundata[which(rundata$workout_type=="1" & rundata$elapsed_time < 90000 & rundata$moving_time<=rundata$elapsed_time),]
+runrace <- rundata[which(rundata$workout_type=="1"),]
 
 #subset non races for run data
-runtrain <- rundata[which(rundata$workout_type!="1" & rundata$elapsed_time < 90000),]
+runtrain <- rundata[which(rundata$workout_type!="1"),]
 
 #create variable containing the difference between moving and elapsed time for races and training runs
 racediff <- runrace$elapsed_time - runrace$moving_time
@@ -23,7 +24,13 @@ traindiff <- runtrain$elapsed_time - runtrain$moving_time
 
 #plot elapsed time vs moving time
 qplot(runrace$moving_time,runrace$elapsed_time,main="Races runs",xlab="Moving time",ylab="Elapsed time")
-qplot(runtrain$moving_time,runtrain$elapsed_time,main="Training runs",xlab="Moving time",ylab="Elapsed time")
+qplot(runtrain$moving_time,runtrain$elapsed_time,main="Training runs (all)",xlab="Moving time",ylab="Elapsed time")
+
+p1 <- qplot(rundata[which(is.na(rundata$workout_type)),]$moving_time,rundata[which(is.na(rundata$workout_type)),]$elapsed_time,main="Training runs (NA)",xlab="Moving time",ylab="Elapsed time")
+p2 <- qplot(runtrain[which(runtrain$workout_type=="0"),]$moving_time,runtrain[which(runtrain$workout_type=="0"),]$elapsed_time,main="Training runs (Default)",xlab="Moving time",ylab="Elapsed time")
+p3 <- qplot(runtrain[which(runtrain$workout_type=="2"),]$moving_time,runtrain[which(runtrain$workout_type=="2"),]$elapsed_time,main="Training runs (Long run)",xlab="Moving time",ylab="Elapsed time")
+p4 <- qplot(runtrain[which(runtrain$workout_type=="3"),]$moving_time,runtrain[which(runtrain$workout_type=="3"),]$elapsed_time,main="Training runs (Intervals)",xlab="Moving time",ylab="Elapsed time")
+grid.arrange(p1,p2,p3,p4,ncol=2)
 
 #histogram of difference between elapsed and moving times, first bin removed to adjust scale
 qplot(racediff,gemom="histogram",ylim=c(0,100),xlab="Elapsed time-moving time",main="Race runs")
